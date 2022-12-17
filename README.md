91c0aed95f46685daab39029d2b2fcf3e7051c14
Michael Dausmann <mdausmann@gmail.com>
2022-12-17 15:03:58 +1100

    Initial commit, day 1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of code 2022 in Rust by Michael Dausmann
+
+All exercises are examples.  To run the second part of day 1 :-
+
+```
+cargo run --example ex1b
+```
---
+++ examples/ex1.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let mut current_max_agg = 0;
+    let mut current_agg = 0;
+    if let Ok(lines) = read_lines("./input/input_1.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+              if ip.len() == 0 {
+                if current_agg > current_max_agg {
+                  current_max_agg = current_agg;
+                }
+                current_agg = 0;
+              } else {
+                let my_int = ip.parse::<i32>().unwrap();
+                current_agg += my_int;
+              }
+              println!("{}", ip);
+            }
+        }
+        println!("Final max {}", current_max_agg);
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
